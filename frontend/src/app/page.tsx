c8ac55a5b4fe7b09c47f2074ab8f719f4617eb33
tanishkag237 <tgoel029@gmail.com>
2025-11-04 22:14:38 +0530

    homepage authbtn change

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -8,7 +8,7 @@ export default function HomePage() {
      <div style={{ background: 'linear-gradient(45deg, #0b1120 100%, #1a2a4c 0%)' }} className="  p-4  ">
       <div className="flex justify-between p-2">
         <Image src="/Logo52.png" alt="Logo" width={150} height={150} className="mt-0" />
-        <AuthButton showStatus={true}>Get Started</AuthButton>
+        <AuthButton>Get Started</AuthButton>
       </div>
 
       <div className="flex justify-between items-center mt-5 p-3">
